5df6b220f9ae960377e733db71f0ab7cebf76771
Robin Karlsson <robink90@gmail.com>
2022-04-13 15:44:50 +0200

    Lagt in footer-komponent, css fil, gjort footer sticky och uppdaterat gitignore

---
+++ src/app/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class Footer extends React.Component {
+    render() {
+        return (
+            <footer>
+                <p>CrowdTest &copy; Robin Karlsson (WU21)</p>
+            </footer>
+        );
+    }
+}
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,4 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import Footer from "./components/footer.jsx"
 
-ReactDOM.render(<p>Hello</p>, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+<Footer />
+);
