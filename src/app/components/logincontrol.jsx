15152efb7bfbce34b4d5565b91754c5383f753e2
Robin Karlsson <robink90@gmail.com>
2022-04-21 09:06:56 +0200

    Uppdaterat logincontrol att använda state istället för api

--- src/app/components/logincontrol.jsx
+++ src/app/components/logincontrol.jsx
@@ -6,10 +6,10 @@ export class LoginControl extends React.Component {
         super(props);
         this.handleLoginClick = this.handleLoginClick.bind(this);
         this.handleLogoutClick = this.handleLogoutClick.bind(this);
-        this.state = {isLoggedIn: false};
+        this.state = {isLoggedIn: false, userRole:'', currentUser:''};
     }
     handleLoginClick(setAccount, setRole) {
-        this.setState({isLoggedIn: true});
+        this.setState({isLoggedIn: true, currentUser: setAccount, userRole: setRole});
         api.loginUser(setAccount, setRole);
     }
     handleLogoutClick() {
@@ -17,24 +17,21 @@ export class LoginControl extends React.Component {
         api.logoutUser();
     }
     render() {
-        const loginasTester = "tester";
-        const loginasCompany = "company";
-
         if (this.state.isLoggedIn === false) {
             return (
-                <><button id="btnLat" className="btn btn-success" onClick={() => this.handleLoginClick("testkonto-testare",loginasTester)}>Logga in som testare</button><br></br><br></br>
-                <button id="btnLac" className="btn btn-success" onClick={() => this.handleLoginClick("testkonto-företag",loginasCompany)}>Logga in som företag</button></>
+                <><button id="btnLat" className="btn btn-success" onClick={() => this.handleLoginClick("testkonto-testare","tester")}>Logga in som testare</button><br></br><br></br>
+                <button id="btnLac" className="btn btn-success" onClick={() => this.handleLoginClick("testkonto-företag","company")}>Logga in som företag</button></>
             );
-            } else if (api.currentRole() === loginasCompany){
+            } else if (this.state.userRole === "company"){
                 return(
-                <><button id="btnLo" className="btn btn-danger" onClick={this.handleLogoutClick}>Logga ut som {api.currentUser()}</button><br></br><br></br>
+                <><button id="btnLo" className="btn btn-danger" onClick={this.handleLogoutClick}>Logga ut som {this.state.currentUser}</button><br></br><br></br>
                 <button id="btnAf" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#adformModal">
-                        Lägg till ny annons
+                    Lägg till ny annons
                 </button></>
                 );
             } else {
             return (
-                <button id="btnLo" className="btn btn-danger" onClick={this.handleLogoutClick}>Logga ut som {api.currentUser()}</button>
+                <button id="btnLo" className="btn btn-danger" onClick={this.handleLogoutClick}>Logga ut som {this.state.currentUser}</button>
             );
         }
     }
