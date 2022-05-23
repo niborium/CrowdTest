48d8f43b52d1bcd67563c54c5c255c5c59022126
Robin Karlsson <robink90@gmail.com>
2022-05-23 10:56:54 +0200

    Ändra om kommentartexten i button.jsx, konstigt formulerad.

--- src/app/reusable-components/button.jsx
+++ src/app/reusable-components/button.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 {/*
 Komponenten som görs återanvändbar i detta projekt är för Button. 
-Vi fick situationen att vi måste skapa olika typer av knappar efter projektets behov. 
+Jag fick situationen att jag måste skapa olika typer av knappar efter projektets behov. 
 Så den återanvändbara komponenten är gjord för den som kan användas var som helst i appen genom att importera den.
-För att använda den här återanvändbara knappen bör vi importera den till komponentfilen där vi behöver knappen. 
-Sedan kan vi skicka props till den enligt kraven. onClick-funktionalitet kan också läggas till den helt enkelt genom att skicka props. 
-Allt som skickas till text props kommer att visas som knapp. Vi kan skicka className, css eller vad som helst som props till den.
+För att använda den här återanvändbara knappen bör man importera den till komponentfilen där man behöver knappen. 
+Sedan kan man skicka props till den enligt kraven. onClick-funktionalitet kan också läggas till den helt enkelt genom att skicka props. 
+Allt som skickas till text props kommer att visas som knapp. Man kan skicka className, css eller vad som helst som props till den.
 */}
 
 const Button = (props) => {
