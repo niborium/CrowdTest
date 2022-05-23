82575eb20baa14274bdfde30d3f17e5801004cad
Robin Karlsson <robink90@gmail.com>
2022-05-23 09:05:58 +0200

    Lagt till kommentar i errorboundry.jsx som beskriver error boundry komponenten

--- src/app/ErrorBoundry/errorboundry.jsx
+++ src/app/ErrorBoundry/errorboundry.jsx
@@ -1,5 +1,17 @@
 import React, { Component } from 'react';
 
+{/*
+This component has three states hasError, error and errorInfo.
+When an error occurs, the getDerivedStateFromError () function is called, which sets the hasError state to true, 
+and based on this state, we return this component. 
+We can also trigger the componentDidCatch lifecycle method to access the error and the information 
+to know more about why the error occurred.
+
+Using Error boundry in the project helps prevent the app from crashing when it encounters the error.
+Instead, it can be implemented to show where we encountered errors, 
+and we can work on fixing the error based on error information from the componentDidCatch life cycle method.
+*/}
+
 export class Errorboundry extends Component {
   constructor(props) {
     super(props);
