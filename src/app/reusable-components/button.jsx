5f5928c7679ff5c5b17dbdbb9da496f88199ea3f
Robin Karlsson <robink90@gmail.com>
2022-05-23 09:11:00 +0200

    Lagt till kommentar i button.jsx som beskriver reuseable komponenten. Tog även bort kommentar från errorboundry.jsx som var ej nödvändig.

--- src/app/reusable-components/button.jsx
+++ src/app/reusable-components/button.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+{/*
+Komponenten som görs återanvändbar i detta projekt är för Button. 
+Vi fick situationen att vi måste skapa olika typer av knappar efter projektets behov. 
+Så den återanvändbara komponenten är gjord för den som kan användas var som helst i appen genom att importera den.
+För att använda den här återanvändbara knappen bör vi importera den till komponentfilen där vi behöver knappen. 
+Sedan kan vi skicka props till den enligt kraven. onClick-funktionalitet kan också läggas till den helt enkelt genom att skicka props. 
+Allt som skickas till text props kommer att visas som knapp. Vi kan skicka className, css eller vad som helst som props till den.
+*/}
+
 const Button = (props) => {
   return (
     <>
